036565b5899c516e81323c44e2a0510a9ee1998d
erikadelgado455 <erikadelgado455@yahoo.com>
2018-07-24 21:32:57 -0400

    i added a function that generates a loss based on timeout, a function that generates a win based on the correct answer, and a function that generates a loss based on a incorrect guess

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function() {
+    var prettygood1=document.getElementById("prettygood");
+    
+    
+
+
     //function that creates the start button screen
     
     function initialScreen() {
@@ -12,11 +17,53 @@ $(document).ready(function() {
     //function that is triggered by the start button, and generates the HTML 
     $("body").on("click", ".start-button", function(event){
         event.preventDefault(); 
-        clickSound.play();
         generateHTML();
-    
         timerWrapper();
+        
     
+    });
+    $("body").on("click", ".answer", function(event){
+        
+    
+    selectedAnswer = $(this).text();
+    if(selectedAnswer === correctAnswers[questionCounter]) {
+        clearInterval(theClock);
+        generateWin();
+        
+        
+    }
+    else {
+        clearInterval(theClock);
+        generateLoss();
+    }
     }); 
     
-})
+    $("body").on("click", ".reset-button", function(event){
+    
+    resetGame();
+    }); 
+    
+    });  
+    
+    function generateLossDueToTimeOut() {
+    unansweredTally++;
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/wrong.gif'>";
+    $(".mainArea").html(gameHTML);
+    setTimeout(wait, 4000);  
+    }
+    
+    function generateWin() {
+    correctTally++;
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+    $(".mainArea").html(gameHTML);
+    setTimeout(wait, 4000);  
+    }
+    
+    function generateLoss() {
+    incorrectTally++;
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/wrong.gif'>";
+    $(".mainArea").html(gameHTML);
+    setTimeout(wait, 4000); 
+    }
+    
+    
