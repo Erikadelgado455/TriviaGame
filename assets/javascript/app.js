f1d48ba77f227394673f0ec5429f106353bfee47
erikadelgado455 <erikadelgado455@yahoo.com>
2018-07-26 16:13:53 -0400

    i finally got my timer to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
 var seconds=60;
 var counter=setInterval(timer, 1000);
 function timer()
-{count=count-1;
-  if (count <= 0)
+{seconds=seconds-1;
+  if (seconds <= 0)
   {
      clearInterval(counter);
      return;
   }
 
- document.getElementById("timer").innerHTML=count + " secs"; 
+ document.getElementById("timer").innerHTML=seconds + " secs"; 
 }
