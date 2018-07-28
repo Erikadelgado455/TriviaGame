5af2b26a05f336fc59ef11ead30a256a603423d2
erikadelgado455 <erikadelgado455@yahoo.com>
2018-07-28 00:22:07 -0400

    i created 8 if statements that add an increment if the selected answer is correct, i added an alert that states the time has ran out,that automactically resets the quiz, and after you hit th submit button an alert shows how many is correct and a option to replay the quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,61 @@
 
-var seconds=60;
+var seconds=120;
 var counter=setInterval(timer, 1000);
 function timer()
 {seconds=seconds-1;
 if (seconds <= 0)
 {
+    answers();
+    location.reload();
     clearInterval(counter);
     return;
+
+    
 }
 
 document.getElementById("timer").innerHTML=seconds + " secs"; 
 };
 function answers(){
 
-	var question1=document.trivia-quiz.question1.value;
-	var question2=document.trivia-quiz.question2.value;
-    var question3=document.trivia-quiz.question3.value;
-    var question4=document.trivia-quiz.question4.value;
-    var question5=document.trivia-quiz.question5.value;
-    var question6=document.trivia-quiz.question6.value;
-    var question7=document.trivia-quiz.question7.value;
-    var question8=document.trivia-quiz.question8.value;
-	var correct= 0;
+	var question1=document.getElementById("trivia-quiz").question1.value;
+	var question2=document.getElementById("trivia-quiz").question2.value;
+    var question3=document.getElementById("trivia-quiz").question3.value;
+    var question4=document.getElementById("trivia-quiz").question4.value;
+    var question5=document.getElementById("trivia-quiz").question5.value;
+    var question6=document.getElementById("trivia-quiz").question6.value;
+    var question7=document.getElementById("trivia-quiz").question7.value;
+    var question8=document.getElementById("trivia-quiz").question8.value;
+	var correct=0;
 
 
-	
+	if (question1=="P. Sherman 42 Wallaby Way Sidney") {
+		correct++;
+    }
+	if (question2=="Johnny Depp") {
+		correct++;
+    }
+    if (question3=="Sleeping Beauty") {
+		correct++;
+    }
+    if (question4=="Iruka Sensei") {
+		correct++;
+    }
+    if (question5=="1982") {
+		correct++;
+    }
+    if (question6=="Hugh Jackman") {
+		correct++;
+    }
+    if (question7=="Elijah Wood") {
+		correct++;
+    }
+    if (question8=="Frank") {
+		correct++;
+    }
+    alert(correct + " correct!")
+    if(confirm("would you like to retry?")){   
+        location.reload();
+    } else{
+
+    }
 }
